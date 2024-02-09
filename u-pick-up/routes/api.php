2dba432085824bf576d54b5871d11cfaae1c972d
jairasolis <jairasolis030@gmail.com>
2024-02-09 17:19:12 +0800

    routes

--- u-pick-up/routes/api.php
+++ u-pick-up/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\AdminController;
+
 
 
 /*
@@ -21,3 +23,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('/student', StudentController::class);
+
+Route::apiResource('/admin', AdminController::class);
