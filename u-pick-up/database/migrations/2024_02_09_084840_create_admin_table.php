502584555678e15cdee7c1bd3db5eafc211fbfd7
jairasolis <jairasolis030@gmail.com>
2024-02-09 16:56:25 +0800

    admin

---
+++ u-pick-up/database/migrations/2024_02_09_084840_create_admin_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admin', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('email_ad');
+            $table->date('username');
+            $table->date('department');
+            $table->date('password');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admin');
+    }
+};
