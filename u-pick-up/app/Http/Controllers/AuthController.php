9b648ef422ef72d1b7f6421b7881f930d8d475d7
Monica Villanueva <vllnv.monica@gmail.com>
2024-02-10 16:18:09 +0800

    imported student model in auth controller

--- u-pick-up/app/Http/Controllers/AuthController.php
+++ u-pick-up/app/Http/Controllers/AuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Models\User;
+use App\Models\Student;
 use App\Models\Admin;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
