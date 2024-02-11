def73b1558949c12faf1029fc263a66e1d72151a
jairasolis <jairasolis030@gmail.com>
2024-02-11 14:51:04 +0800

    modules and uniform BE-->FE

--- u-pick-up-frontend/src/pages/admin pages/inventory/inventory items/Modules.jsx
+++ u-pick-up-frontend/src/pages/admin pages/inventory/inventory items/Modules.jsx
@@ -1,94 +1,75 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './Modules.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios'
 
 
 const Modules = () => {
+
+  const [modulesData, setModulesData] = useState([]);
+  useEffect(() => {
+      fetchData();
+  }, [])
+
+  const fetchData = async () => {
+      try {
+          const result = await axios("https://u-pick-up-y7qnw.ondigitalocean.app/api/modules");
+          console.log(result.data.results);
+          setModulesData(result.data.results)
+      } catch (err) {
+          console.log("somthing Wrong");
+      }
+  }
+
+  const handleDelete=async(id)=>{
+      console.log(id);
+      await axios.delete("https://u-pick-up-y7qnw.ondigitalocean.app/api/booksdelete"+id);
+      const newModulesData=bookData.filter((item)=>{
+          return(
+              item.id !==id
+          )
+      })
+      setModulesData(newModulesData);
+  }
+
   return (
     <div className='modules-page'>
       <div className="modules-container">
         <table>
           <thead>
             <tr>
+              <th> ID </th>
               <th> Subject code </th>
               <th> Subject name </th>
+              <th> Year Level </th>
+              <th> Course </th>
               <th> Available </th>
               <th> Quantity </th>
               <th> Action </th>
             </tr>
           </thead>
           <tbody className='modules'>
-            <tr>
-              <td> ITE 393 </td>
-              <td> Applications Development and Emerging Technologies </td>
-              <td> 100 </td>
-              <td> 100 </td>
-              <td> <FontAwesomeIcon icon={ faPenToSquare } className='action-icon'/> <FontAwesomeIcon icon={ faTrash } className='del-icon'/>  </td>
-            </tr>
-            <tr>
-              <td> ITE 400 </td>
-              <td> System Integration Architecture </td>
-              <td> 100 </td>
-              <td> 100 </td>
-              <td> <FontAwesomeIcon icon={ faPenToSquare } className='action-icon'/> <FontAwesomeIcon icon={ faTrash } className='del-icon'/>  </td>
-            </tr>
-            <tr>
-              <td> ITE 308 </td>
-              <td> Web System and Technologies </td>
-              <td> 100 </td>
-              <td> 100 </td>
-              <td> <FontAwesomeIcon icon={ faPenToSquare } className='action-icon'/> <FontAwesomeIcon icon={ faTrash } className='del-icon'/>  </td>
-            </tr>
-            <tr>
-              <td> ITE 380 </td>
-              <td> Human Computer Interaction 2 </td>
-              <td> 100 </td>
-              <td> 100 </td>
-              <td> <FontAwesomeIcon icon={ faPenToSquare } className='action-icon'/> <FontAwesomeIcon icon={ faTrash } className='del-icon'/>  </td>
-            </tr>
-            <tr>
-              <td> GEN 013 </td>
-              <td> People and Earth's Ecosystems </td>
-              <td> 100 </td>
-              <td> 100 </td>
-              <td> <FontAwesomeIcon icon={ faPenToSquare } className='action-icon'/> <FontAwesomeIcon icon={ faTrash } className='del-icon'/>  </td>
-            </tr>
-            <tr>
-              <td> PED 033 </td>
-              <td> PATHFIT 4 </td>
-              <td> 100 </td>
-              <td> 100 </td>
-              <td> <FontAwesomeIcon icon={ faPenToSquare } className='action-icon'/> <FontAwesomeIcon icon={ faTrash } className='del-icon'/>  </td>
-            </tr>
-            <tr>
-              <td> SSP 006 </td>
-              <td> Student Success Program 2 </td>
-              <td> 100 </td>
-              <td> 100 </td>
-              <td> <FontAwesomeIcon icon={ faPenToSquare } className='action-icon'/> <FontAwesomeIcon icon={ faTrash } className='del-icon'/>  </td>
-            </tr>
-            <tr>
-              <td> GEN 009 </td>
-              <td> The Entrepreneurial Mind </td>
-              <td> 100 </td>
-              <td> 100 </td>
-              <td> <FontAwesomeIcon icon={ faPenToSquare } className='action-icon'/> <FontAwesomeIcon icon={ faTrash } className='del-icon'/>  </td>
-            </tr>
-            <tr>
-              <td> GEN 004	 </td>
-              <td> Readings in Philippine History </td>
-              <td> 100 </td>
-              <td> 100 </td>
-              <td> <FontAwesomeIcon icon={ faPenToSquare } className='action-icon'/> <FontAwesomeIcon icon={ faTrash } className='del-icon'/>  </td>
-            </tr>
-            <tr>
-              <td> GEN 003 </td>
-              <td> Science, Technology and Society </td>
-              <td> 100 </td>
-              <td> 100 </td>
-              <td> <FontAwesomeIcon icon={ faPenToSquare } className='action-icon'/> <FontAwesomeIcon icon={ faTrash } className='del-icon'/>  </td>
-            </tr>
+            {
+              modulesData.map((modules, i) => {
+                return (
+                    <tr key={i}>
+                        <td>{i + 1}</td>
+                        <td>{modules.subject_code} </td>
+                        <td>{modules.subject_name} </td>
+                        <td>{modules.year_level} </td>
+                        <td>{modules.course} </td>
+                        <td>{modules.available} </td>
+                        <td>{modules.quantity} </td>
+                        <td>
+                            {/* <NavLink to={`/view/${book.id}`} className="btn btn-success mx-2">View</NavLink>
+                            <NavLink to={`/edit/${book.id}`} className="btn btn-info mx-2">Edit</NavLink>
+                            <button onClick={()=>handleDelete(user.id)} className="btn btn-danger">Delete</button> */}
+                        </td>
+                    </tr>
+                )
+              })
+            }
           </tbody>
         </table>
       </div>
--- u-pick-up-frontend/src/pages/admin pages/inventory/inventory items/Uniforms.jsx
+++ u-pick-up-frontend/src/pages/admin pages/inventory/inventory items/Uniforms.jsx
@@ -1,41 +1,73 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './Uniforms.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+import axios from 'axios'
 
 
 const Uniforms = () => {
+
+  const [uniformData, setUniformData] = useState([]);
+  useEffect(() => {
+      fetchData();
+  }, [])
+
+  const fetchData = async () => {
+      try {
+          const result = await axios("https://u-pick-up-y7qnw.ondigitalocean.app/api/uniforms");
+          console.log(result.data.results);
+          setUniformData(result.data.results)
+      } catch (err) {
+          console.log("somthing Wrong");
+      }
+  }
+
+  const handleDelete=async(id)=>{
+      console.log(id);
+      await axios.delete("https://u-pick-up-y7qnw.ondigitalocean.app/api/uniform-delete"+id);
+      const newUniformData=uniformData.filter((item)=>{
+          return(
+              item.id !==id
+          )
+      })
+      setUniformData(newUniformData);
+  }
+
   return (
     <div className='uniforms-page'>
       <div className="uniforms-container">
         <table>
           <thead>
             <tr>
-              <th> Uniform </th>
+              <th> ID </th>
+              <th> Uniform Type </th>
+              <th> Year Level </th>
+              <th> Course </th>
               <th> Available </th>
               <th> Quantity </th>
               <th> Action </th>
             </tr>
           </thead>
           <tbody className='uniforms'>
-            <tr>
-              <td> Corporate Uniform </td>
-              <td> 100 </td>
-              <td> 100 </td>
-              <td> <FontAwesomeIcon icon={ faPenToSquare } className='action-icon'/> <FontAwesomeIcon icon={ faTrash } className='del-icon'/>  </td>
-            </tr>
-            <tr>
-              <td> RSO Uniform </td>
-              <td> 100 </td>
-              <td> 100 </td>
-              <td> <FontAwesomeIcon icon={ faPenToSquare } className='action-icon'/> <FontAwesomeIcon icon={ faTrash } className='del-icon'/>  </td>
-            </tr>
-            <tr>
-              <td> University Uniform </td>
-              <td> 100 </td>
-              <td> 100 </td>
-              <td> <FontAwesomeIcon icon={ faPenToSquare } className='action-icon'/> <FontAwesomeIcon icon={ faTrash } className='del-icon'/>  </td>
-            </tr>
+            {
+              uniformData.map((uniform, i) => {
+                return (
+                    <tr key={i}>
+                        <td>{i + 1}</td>
+                        <td>{uniform.uniform_type} </td>
+                        <td>{uniform.year_level} </td>
+                        <td>{uniform.course} </td>
+                        <td>{uniform.available} </td>
+                        <td>{uniform.quantity} </td>
+                        <td>
+                            {/* <NavLink to={`/view/${book.id}`} className="btn btn-success mx-2">View</NavLink>
+                            <NavLink to={`/edit/${book.id}`} className="btn btn-info mx-2">Edit</NavLink>
+                            <button onClick={()=>handleDelete(user.id)} className="btn btn-danger">Delete</button> */}
+                        </td>
+                    </tr>
+                )
+            })
+            }
           </tbody>
         </table>
       </div>
