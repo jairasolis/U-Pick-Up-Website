e7191a5a17ae776dfa4fbab6a851edf17322ccdf
Monica Villanueva <vllnv.monica@gmail.com>
2024-02-18 09:07:37 +0800

    reset mail

---
+++ u-pick-up/resources/views/emails/reset_password.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Reset</title>
+</head>
+<body>
+    <p>Hello,</p>
+    <p>You requested a password reset. Please click the following link to reset your password:</p>
+    <a href="{{ $resetUrl }}">Reset Password</a>
+    <p>If you didn't request a password reset, you can safely ignore this email.</p>
+</body>
+</html>
