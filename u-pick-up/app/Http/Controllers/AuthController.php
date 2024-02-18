568bec3fbf90f14e4f444f45e2b227c3fcf2f1fd
Monica Villanueva <vllnv.monica@gmail.com>
2024-02-18 19:08:23 +0800

    removed double route

--- u-pick-up/app/Http/Controllers/AuthController.php
+++ u-pick-up/app/Http/Controllers/AuthController.php
@@ -149,7 +149,7 @@ class AuthController extends Controller
             ['token' => $token, 'created_at' => now()]
         );
     
-        $resetUrl = 'http://localhost:3000/reset-password/reset-password?token=' . $token;
+        $resetUrl = 'http://localhost:3000/reset-password?token=' . $token;
     
         try {
             Mail::to($request->email)->send(new ResetPasswordMail($resetUrl));
