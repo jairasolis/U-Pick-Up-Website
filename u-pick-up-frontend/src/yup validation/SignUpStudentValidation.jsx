32e2b85235e3b3b08c4f9bddf721b6d9b134899c
jairasolis <jairasolis030@gmail.com>
2024-03-01 21:41:38 +0800

    ..

--- u-pick-up-frontend/src/yup validation/SignUpStudentValidation.jsx
+++ u-pick-up-frontend/src/yup validation/SignUpStudentValidation.jsx
@@ -1,12 +1,6 @@
 import * as Yup from "yup";
 
 export const SignUpStudentValidation = Yup.object({
-  // firstName: Yup.string().required("First name is required."),
-  // middleName: Yup.string().required("Middle name is required."),
-  // lastName: Yup.string().required("Last name is required."),
-  // program: Yup.string().required("Program is required."),
-  // department: Yup.string().required("Department is required."),
-  // gender: Yup.string().required("Gender is required."),
 
   email: Yup.string()
     .matches(
