b77bf9cee26aee1493ed75aa5c268a28c9f8c8a3
Glaiza <glaizadarlenem@gmail.com>
2024-03-05 17:24:33 +0800

    new left navigation bar

--- u-pick-up-frontend/src/components/leftBar/LeftBar.jsx
+++ u-pick-up-frontend/src/components/leftBar/LeftBar.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './LeftBar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse, faCalendar, faUser, faBook } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom';
 
-const LeftBar = () => {
+/*const LeftBar = () => {
   return (
     <div className="leftBar">
       <div className="leftbar-container">
@@ -38,6 +38,58 @@ const LeftBar = () => {
       </div>
     </div>
   )
-}
+}*/
 
-export default LeftBar
+
+const LeftBar = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsActive(!isActive);
+  };
+
+  return (
+    <div className="sidebar">
+      <div className="logo_content">
+        <div className="logo">
+          <i className="fas fa-u"></i>
+          <div className="logo_name">UpickUp</div>
+        </div>
+        <i className="fa fa-arrow-right" id="btn" onClick={toggleSidebar}></i>
+      </div>
+      <ul className="nav_list">
+        <li>
+          <Link to="/student/home">
+            <FontAwesomeIcon icon={faHouse} />
+            <span className="links_name">Home</span>
+          </Link>
+          <span className="tooltip">Home</span>
+        </li>
+        <li>
+          <Link to="/student/calendar">
+            <FontAwesomeIcon icon={faCalendar} />
+            <span className="links_name">Calendar</span>
+          </Link>
+          <span className="tooltip">Calendar</span>
+        </li>
+        <li>
+          <Link to="/student/inventory">
+            <FontAwesomeIcon icon={faBook} />
+            <span className="links_name">Inventory</span>
+          </Link>
+          <span className="tooltip">Inventory</span>
+        </li>
+        <li>
+          <Link to="/student/profile">
+            <FontAwesomeIcon icon={faUser} />
+            <span className="links_name">Profile</span>
+          </Link>
+          <span className="tooltip">Profile</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+
+export default LeftBar;
